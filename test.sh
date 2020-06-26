1f5810811599a863b6305ba674d0f97cdac1e734
Zhihao <zhihao@cs.stanford.edu>
2020-06-26 22:54:55 +0000

    Add an example_run script

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 old="$IFS"
 IFS='_'
 str="$*"
-str=long0904_${str}_fix_reddit.txt
-echo "$str"
+#str=long0904_${str}_fix_reddit.txt
+#echo "$str"
 IFS=$old
-./gnn -ll:gpu 1 -ll:cpu 4 -ll:fsize 12000 -ll:zsize 30000 -lr $1 -decay $2 -decay-rate $3 -dropout $4 -layers $5 -file /scratch/users/zhihao/GNNDatasets/reddit-dgl/reddit-dgl -e $6 -seed $7> results/$str
+./gnn -ll:gpu 1 -ll:cpu 4 -ll:fsize 12000 -ll:zsize 30000 -lr $1 -decay $2 -decay-rate $3 -dropout $4 -layers $5 -file dataset/reddit-dgl -e $6
